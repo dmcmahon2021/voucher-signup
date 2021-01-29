b4738e00c8aba61f2053bb6ce59086d7ebb563d2
Damon McMahon <damon.mcmahon@service.nsw.gov.au>
2021-01-29 11:54:26 +1100

    #176710703 initial commit

---
+++ src/main/kotlin/au/gov/nsw/service/vouchersignup/VoucherSignupApplication.kt
@@ -0,0 +1,11 @@
+package au.gov.nsw.service.vouchersignup
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VoucherSignupApplication
+
+fun main(args: Array<String>) {
+	runApplication<VoucherSignupApplication>(*args)
+}
---
+++ src/test/kotlin/au/gov/nsw/service/vouchersignup/VoucherSignupApplicationTests.kt
@@ -0,0 +1,13 @@
+package au.gov.nsw.service.vouchersignup
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VoucherSignupApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
