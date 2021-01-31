457de18c9046e12e71998ca83d2d4ef83184cea4
Damon McMahon <mcmahon.damonn@gmail.com>
2021-02-01 10:45:46 +1100

    #176710703 add data classes and enable h2 console
    insert sample data in data.sql

--- src/main/kotlin/au/gov/nsw/service/vouchersignup/VoucherSignupApplication.kt
+++ src/main/kotlin/au/gov/nsw/service/vouchersignup/VoucherSignupApplication.kt
@@ -2,6 +2,10 @@ package au.gov.nsw.service.vouchersignup
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import javax.persistence.Embeddable
+import javax.persistence.Embedded
+import javax.persistence.Entity
+import javax.persistence.Id
 
 @SpringBootApplication
 class VoucherSignupApplication
@@ -9,3 +13,25 @@ class VoucherSignupApplication
 fun main(args: Array<String>) {
 	runApplication<VoucherSignupApplication>(*args)
 }
+
+@Entity
+ data class Transaction(
+		@Id
+		var voucherNumber: String? = null,
+		var firstname: String? = null,
+		var lastname: String? = null,
+		var age: Int? = null,
+		var dateOfBirth: String? = null,
+		var email: String? = null,
+		var mobileNumber: String? = null,
+		@Embedded
+		var medicareCard: MedicareCard
+)
+
+@Embeddable
+class MedicareCard {
+	 var number: Long? = null
+	 var expiryMonth: Int? = null
+	 var expiryYear: Int? = null
+}
+
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into TRANSACTION
+values('1234', 29, '01/10/1991', 'damon.mcmahon@service.nsw.gov.au', 'Damon', 'McMahon', 01, 2025, 1234567891,
+       045412440);
