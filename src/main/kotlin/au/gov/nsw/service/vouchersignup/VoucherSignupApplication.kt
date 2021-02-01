4794721844fa366a659baee4b23c4878495ce2b1
Damon McMahon <mcmahon.damonn@gmail.com>
2021-02-01 16:45:18 +1100

    #176710703 add validation
    spring-boot-starter-validation

--- src/main/kotlin/au/gov/nsw/service/vouchersignup/VoucherSignupApplication.kt
+++ src/main/kotlin/au/gov/nsw/service/vouchersignup/VoucherSignupApplication.kt
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import javax.persistence.Embeddable
 import javax.persistence.Embedded
+import javax.validation.Valid
+import javax.validation.constraints.*
 
 @SpringBootApplication
 class VoucherSignupApplication
@@ -16,22 +18,23 @@ fun main(args: Array<String>) {
 }
 
  data class Transaction(
-		var firstname: String? = null,
-		var lastname: String? = null,
-		var age: Int? = null,
-		var dateOfBirth: String? = null,
-		var email: String? = null,
-		var mobileNumber: String? = null,
-		@Embedded
-		var medicareCard: MedicareCard
+		 @field:NotEmpty @field:Size(max = 25) var firstname: String? = null,
+		 @field:NotEmpty @field:Size(max = 25) var lastname: String? = null,
+		 var age: Int? = null,
+		 @field:NotEmpty @field:Pattern(regexp="^(0?[1-9]|[12][0-9]|3[01])[\\/](0?[1-9]|1[012])[\\/\\-]\\d{4}\$")
+		 var dateOfBirth: String? = null,
+		 @field:NotEmpty @field:Email var email: String,
+		 @field:NotEmpty @field:Pattern(regexp="^04[0-9]{8}$") var mobileNumber: String? = null,
+		 @Embedded
+		 var medicareCard: MedicareCard
 )
 
 @Embeddable
-class MedicareCard {
-	 var number: Long? = null
-	 var expiryMonth: Int? = null
-	 var expiryYear: Int? = null
-}
+class MedicareCard (
+	var number: Long? = null,
+	var expiryMonth: Int? = null,
+	var expiryYear: Int? = null
+)
 
 fun getRandomString(length: Int) : String {
 	val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
@@ -44,7 +47,7 @@ fun getRandomString(length: Int) : String {
 class TransactionController {
 
 	@PostMapping("/submission")
-	fun newVoucher(@RequestBody transaction: Transaction): Any {
+	fun newVoucher(@RequestBody @Valid data: Transaction): Any {
 		return object {
 			val voucherNumber = getRandomString(12)
 
