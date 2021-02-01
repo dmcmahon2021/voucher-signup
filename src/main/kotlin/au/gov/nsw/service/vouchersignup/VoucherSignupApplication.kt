44f9e74a2dee9f11f9401cc49cd8998a0431ef31
Damon McMahon <mcmahon.damonn@gmail.com>
2021-02-01 14:21:48 +1100

    #176710703 change to Transaction Class and add Controller
    remove @entity and @Id annotation. Add Post method to return voucherNumber Object

--- src/main/kotlin/au/gov/nsw/service/vouchersignup/VoucherSignupApplication.kt
+++ src/main/kotlin/au/gov/nsw/service/vouchersignup/VoucherSignupApplication.kt
@@ -2,10 +2,11 @@ package au.gov.nsw.service.vouchersignup
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
 import javax.persistence.Embeddable
 import javax.persistence.Embedded
-import javax.persistence.Entity
-import javax.persistence.Id
 
 @SpringBootApplication
 class VoucherSignupApplication
@@ -14,10 +15,7 @@ fun main(args: Array<String>) {
 	runApplication<VoucherSignupApplication>(*args)
 }
 
-@Entity
  data class Transaction(
-		@Id
-		var voucherNumber: String? = null,
 		var firstname: String? = null,
 		var lastname: String? = null,
 		var age: Int? = null,
@@ -34,4 +32,24 @@ class MedicareCard {
 	 var expiryMonth: Int? = null
 	 var expiryYear: Int? = null
 }
+
+fun getRandomString(length: Int) : String {
+	val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
+	return (1..length)
+			.map { allowedChars.random() }
+			.joinToString("")
+}
+
+@RestController
+class TransactionController {
+
+	@PostMapping("/submission")
+	fun newVoucher(@RequestBody transaction: Transaction): Any {
+		return object {
+			val voucherNumber = getRandomString(12)
+
+		}
+	}
+}
+
 
--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into TRANSACTION
-values('1234', 29, '01/10/1991', 'damon.mcmahon@service.nsw.gov.au', 'Damon', 'McMahon', 01, 2025, 1234567891,
-       045412440);
