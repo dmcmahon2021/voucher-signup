dbb0b42eb83e1efab922b05261314d9d454ae567
Damon McMahon <mcmahon.damonn@gmail.com>
2021-02-02 15:12:52 +1100

    #176710703 change project structure
    move class and embedded class to separate class files

--- src/main/kotlin/au/gov/nsw/service/vouchersignup/MedicareCard.kt
+++ src/main/kotlin/au/gov/nsw/service/vouchersignup/MedicareCard.kt
@@ -1,4 +1,12 @@
 package au.gov.nsw.service.vouchersignup
 
-class MedicareCard {
-}
+import javax.persistence.Embeddable
+import javax.validation.constraints.Max
+import javax.validation.constraints.NotEmpty
+
+@Embeddable
+class MedicareCard (
+		@field:NotEmpty @field:Max(10) var number: Long? = null,
+		@field:NotEmpty @field:Max(2) var expiryMonth: Int? = null,
+		@field:NotEmpty @field:Max( 4) var expiryYear: Int? = null
+)
